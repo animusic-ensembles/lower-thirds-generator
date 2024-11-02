d3ca3cfe72bd591b931721222cf53d1b3bd1adf2
Eric Shim <rcshim04@gmail.com>
2024-11-02 14:50:23 -0400

    Initial commit

---
+++ main.py
@@ -0,0 +1,49 @@
+from manim import *
+
+def convert(x, y):
+    x1 = (x - 960) / 135
+    y1 = (540 - y) / 135
+    return [x1, y1, 0]
+def abs_convert(x):
+    return x / 135
+
+class LowerThird(Scene):
+    def construct(self):
+        line = Line(start=convert(100, 900), end=convert(100, 900))
+        line.set_stroke(color=WHITE, width=10)
+        line.generate_target()
+        line.target.put_start_and_end_on(convert(100, 820), convert(100, 980))
+        with register_font('fonts/Roboto-Regular.ttf'):
+            title = Text('Anville Town', font="Roboto", height=abs_convert(60), fill_opacity=0).move_to(convert(-80, 900-30), aligned_edge=LEFT)
+
+        with register_font('fonts/Roboto-Light.ttf'):
+            subtitle = Text('Pokemon Black and White', font="Roboto", height=abs_convert(40), fill_opacity=0).move_to(convert(-80, 900+40), aligned_edge=LEFT)
+                
+        overlay = Rectangle(color=BLACK, height=abs_convert(160), width=abs_convert(100)).move_to(convert(40, 900))
+        overlay.set_fill(color=BLACK, opacity=1)
+
+        title.generate_target()
+        title.target.set_fill(opacity=1)
+        title.target.move_to(convert(150, 900-30), aligned_edge=LEFT)
+        subtitle.generate_target()
+        subtitle.target.set_fill(opacity=1)
+        subtitle.target.move_to(convert(150, 900+40), aligned_edge=LEFT)
+
+        self.add(line)
+        self.play(MoveToTarget(line))
+        self.add(title)
+        self.add(subtitle)
+        self.add(overlay)
+        self.play(MoveToTarget(title), MoveToTarget(subtitle))
+        self.wait(2)
+
+        title.generate_target()
+        title.target.set_fill(opacity=0)
+        title.target.move_to(convert(-80, 900-30), aligned_edge=LEFT)
+        subtitle.generate_target()
+        subtitle.target.set_fill(opacity=0)
+        subtitle.target.move_to(convert(-80, 900+40), aligned_edge=LEFT)
+        line.generate_target()
+        line.target.put_start_and_end_on(convert(100, 900), convert(100, 900))
+        self.play(MoveToTarget(title), MoveToTarget(subtitle))
+        self.play(MoveToTarget(line))
