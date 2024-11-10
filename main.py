f12f9401cd322505732753223a5c28b8e4cf50ae
rcshim04 <rcshim04@gmail.com>
2024-11-10 01:43:19 -0500

    Implement dynamic generation from file

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from manim import *
+import csv
 
 def convert(x, y):
     x1 = (x - 960) / 135
@@ -7,17 +8,26 @@ def convert(x, y):
 def abs_convert(x):
     return x / 135
 
+with open('setlist.csv', 'r', encoding='utf-8') as file:
+    data = list(csv.reader(file))
+
 class LowerThird(Scene):
+
     def construct(self):
+        for line in data:
+            self.next_section(line[0])
+            self.generate(line[0], line[1])
+
+    def generate(self, title_text, subtitle_text):
         line = Line(start=convert(100, 900), end=convert(100, 900))
         line.set_stroke(color=WHITE, width=10)
         line.generate_target()
         line.target.put_start_and_end_on(convert(100, 820), convert(100, 980))
         with register_font('fonts/Roboto-Regular.ttf'):
-            title = Text('Anville Town', font="Roboto", height=abs_convert(60), fill_opacity=0).move_to(convert(-80, 900-30), aligned_edge=LEFT)
+            title = Text(title_text, font="Roboto", height=abs_convert(60), fill_opacity=0).move_to(convert(-80, 900-30), aligned_edge=LEFT)
 
         with register_font('fonts/Roboto-Light.ttf'):
-            subtitle = Text('Pokemon Black and White', font="Roboto", height=abs_convert(40), fill_opacity=0).move_to(convert(-80, 900+40), aligned_edge=LEFT)
+            subtitle = Text(subtitle_text, font="Roboto", height=abs_convert(40), fill_opacity=0).move_to(convert(-80, 900+40), aligned_edge=LEFT)
                 
         overlay = Rectangle(color=BLACK, height=abs_convert(160), width=abs_convert(100)).move_to(convert(40, 900))
         overlay.set_fill(color=BLACK, opacity=1)
